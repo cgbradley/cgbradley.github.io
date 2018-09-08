6ebf0cb0148ccd808ad17004df4d1079abd7bd4b
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 15:48:13 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,1 +1,1 @@
-document.write("<div class="maintitle"><h1>cgbradley.com</h1></div><div class="navbar"><a class="active" href="">Home</a><a href="games">Games</a><a href="images">Images</a><a href="contact">Contact</a><a href="about">About</a></div>");
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
