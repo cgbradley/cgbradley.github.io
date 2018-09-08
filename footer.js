c3454c13338f8cbf2b147f884562d68fb5f10063
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 17:01:45 -0500

    Update footer.js

--- footer.js
+++ footer.js
@@ -1,2 +1,2 @@
 //Imports footer
-document.write("<footer><center><a href=\"/privacy\">Privacy Policy</a> © 2018 Christopher Bradley</center></footer>");
+document.write("<footer><div class=\"footer-block\"><center><a href=\"/privacy\">Privacy Policy</a> © 2018 Christopher Bradley</center></div></footer>");
