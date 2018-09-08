d09b4dad448eee59e1cac4c7c24d64941324f069
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 16:37:57 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,18 +1,21 @@
 //find out what page the user is on
-var page = window.location.pathname;
+var page = window.location.href;
 if(page.includes("games")){
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a class=\"active\" href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+alert("games");
 }
 else if(window.location.pathname == '/'){
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+alert("home");
 }
 else if(page.includes("about")){
-document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a id="" >Home</a><a class=\"active\" href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
-
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+alert("about");
 }
 else if(page.includes("images")){
-        
+alert("images");        
 }
 else{
   document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+alert("unknown pg");
 }
