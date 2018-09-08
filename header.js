478fb4a5cfc8309a89d7b9307102da0313c06806
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 17:23:34 -0500

    Update header.js

--- header.js
+++ header.js
@@ -4,22 +4,18 @@ var page = window.location.href;
 if(page.includes("games")){
 //On Games
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"/ \">Home</a><a class=\"active\" href=\"/games\">Games</a><a href=\"/images\">Images</a><a href=\"/contact\">Contact</a><a href=\"/about\">About</a></div>");
-alert("games");
 }
 else if(window.location.pathname == '/'){
 //At Home
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a href=\"/games\">Games</a><a href=\"/images\">Images</a><a href=\"/contact\">Contact</a><a href=\"/about\">About</a></div>");
-alert("home");
 }
 else if(page.includes("about")){
 //On About
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"/ \" >Home</a><a href=\"/games\">Games</a><a href=\"/images\">Images</a><a href=\"/contact\">Contact</a><a class=\"active\" href=\"/about\">About</a></div>");
-alert("about");
 }
 else if(page.includes("images")){
 //On Images
-document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"/ \" >Home</a><a href=\"/games\">Games</a><a class=\"active\" href=\"/images\">Images</a><a href=\"/contact\">Contact</a><a href=\"/about\">About</a></div>");
-alert("images");        
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"/ \" >Home</a><a href=\"/games\">Games</a><a class=\"active\" href=\"/images\">Images</a><a href=\"/contact\">Contact</a><a href=\"/about\">About</a></div>");      
 }
 else if(page.includes("contact")){
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"/ \" >Home</a><a href=\"/games\">Games</a><a href=\"/images\">Images</a><a class=\"active\" href=\"/contact\">Contact</a><a href=\"/about\">About</a></div>");
