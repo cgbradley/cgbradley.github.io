3fc7f5abedbf06beca8dd977ebbf357fdbcaaa3c
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 16:47:49 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,21 +1,27 @@
 //find out what page the user is on
 var page = window.location.href;
 if(page.includes("games")){
-document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a class=\"active\" href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+//On Games
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"\ \">Home</a><a class=\"active\" href=\"\games\">Games</a><a href=\"\images\">Images</a><a href=\"\contact\">Contact</a><a href=\"\about\">About</a></div>");
 alert("games");
 }
 else if(window.location.pathname == '/'){
+//At Home
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
 alert("home");
 }
 else if(page.includes("about")){
-document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+//On About
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"\ \" >Home</a><a href=\"\games\">Games</a><a href=\"\images\">Images</a><a href=\"\contact\">Contact</a><a class=\"active\" href=\"\about\">About</a></div>");
 alert("about");
 }
 else if(page.includes("images")){
+//On Images
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"\ \" >Home</a><a href=\"\games\">Games</a><a class=\"active\" href=\"\images\">Images</a><a href=\"\contact\">Contact</a><a href=\"\about\">About</a></div>");
+
 alert("images");        
 }
 else{
-  document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+  document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"\ \" >Home</a><a href=\"\games\">Games</a><a href=\"\images\">Images</a><a href=\"\contact\">Contact</a><a href=\"\about\">About</a></div>");
 alert("unknown pg");
 }
