3e5aedaf6794c0f6f0898e7d7882b086315afaa7
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 16:26:14 -0500

    Update header.js

--- header.js
+++ header.js
@@ -1,13 +1,14 @@
 //find out what page the user is on
 var page = window.location.pathname;
 if(page.includes("games")){
-
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a class=\"active\" href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
 }
 else if(window.location.pathname == '/'){
 document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a class=\"active\" >Home</a><a href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
 }
 else if(page.includes("about")){
-  
+document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a id="" >Home</a><a class=\"active\" href=\"games\">Games</a><a href=\"images\">Images</a><a href=\"contact\">Contact</a><a href=\"about\">About</a></div>");
+
 }
 else if(page.includes("images")){
         
