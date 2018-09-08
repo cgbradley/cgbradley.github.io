6c777516d795a9859360b472d6e576ea8df2b19b
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 14:54:24 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,14 +18,6 @@
 
 <body>
     <?php include ("header.php"); ?>
-    <div class="maintitle"><h1>cgbradley.com</h1></div>
-    <div class="navbar">
-      <a class="active" href="">Home</a>
-      <a href="games">Games</a>
-      <a href="images">Images</a>
-      <a href="contact">Contact</a>
-      <a href="about">About</a>
-    </div>
  
   <div class="text">
     <center><h1>Welcome!</h1>
