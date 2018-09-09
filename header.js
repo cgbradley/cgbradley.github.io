1fbdf5b5690ea9f526d23604411e16b61ed42324
cgbradley <32231928+cgbradley@users.noreply.github.com>
2018-09-08 20:22:29 -0500

    Update header.js

--- header.js
+++ header.js
@@ -22,5 +22,5 @@ document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=
 }
 else{
   document.write("<div class=\"maintitle\"><h1>cgbradley.com</h1></div><div class=\"navbar\"><a href=\"/ \" >Home</a><a href=\"/games\">Games</a><a href=\"/images\">Images</a><a href=\"/contact\">Contact</a><a href=\"/about\">About</a></div>");
-alert("unknown pg");
+
 }
